e887ff7ecb2da2bc8b0bd5b0412e6ba556987819
AlecRoss01 <61425095+AlecRoss01@users.noreply.github.com>
2020-10-22 23:47:02 -0400

    Update project2.sh

--- project2.sh
+++ project2.sh
@@ -13,21 +13,29 @@ if [ "$(id -u)" = "0" ]; then
         while read line; do
             user=$(echo ${line} | cut -d "@" -f 1)
             pass=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
+            #my original random didn't want to work with me, so I searched around and found one that was specifically characters, rather than openssl which i used originally
+            #https://unix.stackexchange.com/questions/230673/how-to-generate-a-random-string
             temp=$(echo $(grep "${user}" /etc/passwd | cut -d ":" -f 1))
             if [[ ${temp} == ${user} ]]; then
                 echo "${user} alread exists"
                 echo "${pass} is new password"
                 echo -e "${pass}\n${pass}" | passwd ${user}
+                #had issues changing passwords for alread created users, then I used a trick I found here
+                #https://www.systutorials.com/changing-linux-users-password-in-one-command-line/
                 chage --lastday 0 ${user}
                 usermod -a -G CSI230 ${user}
                 usermod --shell /bin/bash ${user}
             else
                 echo "${user} does not exist, creating new user"
+                #https://www.tecmint.com/add-users-in-linux/#:~:text=To%20add%2Fcreate%20a%20new,already%20exists%20on%20the%20system).
+                #first iteration of adding users, didn't go so well, but I undesrtood how
                 echo "Password is : ${pass}"
                 useradd ${user}
                 echo -e "${pass}\n${pass}" | passwd ${user}
                 usermod -a -G CSI230 ${user}
                 chage --lastday 0 ${user}
+                #chage evaded me for a bit but eventually I came to undesrtand it after looking over the online man page equivilent of it
+                #https://linux.die.net/man/1/chage
                 usermod --shell /bin/bash ${user}
             fi
         done <${file}
